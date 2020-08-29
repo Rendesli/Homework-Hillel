d04515c03a91db1e2dccbe31ccc0a0f610c1f85b
Rendesli <69943533+Rendesli@users.noreply.github.com>
2020-08-29 18:02:28 +0300

    Correct_HW5

--- index.js
+++ index.js
@@ -1,17 +1,11 @@
-function ret(){
+function Type(a) {
+    if (typeof a === "number") {
+        return ("1");
+    } else if (typeof a === "string") {
+        return ("0");
+    } else {
+        return ("-1");
+    }
+}
 
-    let a = prompt("введите что-то?")
-
-let reN = /(^-?\d+$)/.test(a);
-let reS=/(^-?[A-Za-z]+$)/.test(a);
-if(reN){
-    return alert("1");
-}else{
-    if(reS){
-    return alert("0");
-}else{
-    return alert("-1");
-}
-}
-}
-ret();
+Type(Boolean);
