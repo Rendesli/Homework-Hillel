7b495049aaf09de01d9c8199804e2b3c1876e1a5
Rendesli <69943533+Rendesli@users.noreply.github.com>
2020-08-29 19:28:09 +0300

    OPTIONAL_HW3_CALCULETE

---
+++ optional_hw3.js
@@ -0,0 +1,38 @@
+var n1 = parseInt(prompt("First number:"));
+var sign = prompt("sign");
+var n2 = parseInt(prompt("Second number:"));
+
+
+switch (sign) {
+     case "+": {
+          function sum() {
+               result = n1 + n2;
+               alert(result);
+          }
+          sum();
+     }
+     break;
+     case "-": {
+     function sub() {
+          result = n1 - n2;
+          alert(result);
+     }
+     sub();
+     }break;
+     case "*": {
+          function multi() {
+               result = n1 * n2;
+               alert(result);
+          }
+          multi();
+          }break;
+          case "/": {
+               function dev() {
+                    result = n1 / n2;
+                    alert(result);
+               }
+               dev();
+               }break;
+               default:
+                    alert("Error!");
+}
