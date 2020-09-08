b652dd9fa18787a83c8fc37ffabd7f5ddbf67de2
Rendesli <69943533+Rendesli@users.noreply.github.com>
2020-09-08 14:31:44 +0300

    Update MyMap.js

--- LESSON_5/MyMap.js
+++ LESSON_5/MyMap.js
@@ -1,4 +1,26 @@
-//Исправленно --------------------------------
+//-----uppercase----
+let str = 'Что разум человека может постигнуть и во что он может поверить, того он способен достичь.';
+
+let strarr = str.split(" ");
+
+function myMapCallback(arr, cb) {
+    let arrLength = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        let item = arr[i];
+        arrLength[i] = cb(item, i, arr).toUpperCase(arrLength);
+    }
+    return arrLength;
+}
+
+const result = myMapCallback(strarr, (item) => {
+    return item;
+})
+console.log(result);
+
+
+
+//Исправленно////не правильно-------------------------------
 
 let str = 'Что разум человека может постигнуть и во что он может поверить, того он способен достичь.';
 
