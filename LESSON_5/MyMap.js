ebed2608c138c724e00d05d80b30e7c356fa5fa4
Rendesli <69943533+Rendesli@users.noreply.github.com>
2020-09-08 22:21:24 +0300

    I_can't_stand

--- LESSON_5/MyMap.js
+++ LESSON_5/MyMap.js
@@ -1,6 +1,4 @@
-//-----uppercase----
 let str = 'Что разум человека может постигнуть и во что он может поверить, того он способен достичь.';
-
 let strarr = str.split(" ");
 
 function myMapCallback(arr, cb) {
@@ -13,30 +11,8 @@ function myMapCallback(arr, cb) {
     return arrLength;
 }
 
-const result = myMapCallback(strarr, (item) => {
-    return item;
-})
-console.log(result);
+console.log(myMapCallback(strarr,( item=>{ return item })));
+console.log(strarr.map((item)=> item.toUpperCase()));
 
 
 
-//Исправленно////не правильно-------------------------------
-
-let str = 'Что разум человека может постигнуть и во что он может поверить, того он способен достичь.';
-
-let strarr = str.split(" ");
-
-function myMapCallback(arr, cb) {
-    let arrLength = [];
-
-    for (let i = 0; i < arr.length; i++) {
-        const item = arr[i].length;
-        arrLength[i] = cb(item, i, arr);
-    }
-    return arrLength;
-}
-
-const result = myMapCallback(strarr, (item) => {
-    return item;
-})
-console.log(result);
