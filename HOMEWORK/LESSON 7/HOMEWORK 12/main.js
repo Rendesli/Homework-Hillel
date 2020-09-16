6ee388bfa686b1d3d8232cd19ea018100a2d5633
Rendesli <69943533+Rendesli@users.noreply.github.com>
2020-09-16 12:20:41 +0300

    Update main.js

--- HOMEWORK/LESSON 7/HOMEWORK 12/main.js
+++ HOMEWORK/LESSON 7/HOMEWORK 12/main.js
@@ -4,4 +4,4 @@ function  filterUpperCase(world){
 
   return [].filter.call(word, (item) => item === item.toUpperCase() ? item : false);
 }
-console.log(filterUpperCase(world));
+console.log(filterUpperCase(word));
