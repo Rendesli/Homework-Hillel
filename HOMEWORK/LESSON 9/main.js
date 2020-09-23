b04f77720a9d25e79069c4acecacc05f03bb6305
Rendesli <69943533+Rendesli@users.noreply.github.com>
2020-09-23 17:37:34 +0300

    Update main.js

--- HOMEWORK/LESSON 9/main.js
+++ HOMEWORK/LESSON 9/main.js
@@ -1,76 +1,76 @@
-//--------human---
 function Human(config) {
     this.name = config.name;
     this.surname = config.surname;
     this.age = config.age;
 }
 
-//-------students-----
+
+
+let student1 = {
+    name: 'Vova',
+    surname: 'Lebedev',
+    age: 17,
+    marks: [7, 9, 10, 6, 4]
+};
+let student2 = {
+    name: 'Katya',
+    surname: 'Golden',
+    age: 16,
+    marks: [7, 4, 2, 9, 8]
+};
+let student3 = {
+    name: 'Vanya',
+    surname: 'Kalin',
+    age: 18,
+    marks: [4, 5, 7, 1, 9]
+};
+let student4 = {
+    name: 'Sasha',
+    surname: 'Shelbi',
+    age: 19,
+    marks: [3, 7, 8, 2, 4]
+};
+let student5 = {
+    name: 'Masha',
+    surname: 'Holms',
+    age: 21,
+    marks: [9, 6, 2, 10, 4]
+};
+
+let group = [
+    new Student(student1), new Student(student2), new Student(student3), new Student(student4), new Student(student5)
+];
+
+function Teacher(config) {
+    Human.call(this, config);
+    this.group = config.group;
+
+}
+
 function Student(config) {
     Human.call(this, config);
     this.marks = config.marks;
 };
 
-//----teacher----
-function Teacher(config) {
-    Human.call(this, config);
-    this.group = [new Student({
-            name: 'Sasha',
-            surname: 'Lebedev',
-            age: 16,
-            marks: [3, 8, 10, 4, 6],
-        }),
-        new Student({
-            name: 'Masha',
-            surname: 'Golden',
-            age: 17,
-            marks: [7, 7, 9, 10, 2],
-        }),
-        new Student({
-            name: 'Vanya',
-            surname: 'Kalin',
-            age: 18,
-            marks: [10, 3, 7, 5, 8],
-        }),
-        new Student({
-            name: 'Katya',
-            surname: 'Shelbi',
-            age: 18,
-            marks: [4, 5, 9, 10, 3],
-        }),
-        new Student({
-            name: 'Vova',
-            surname: 'Holms',
-            age: 19,
-            marks: [9, 7, 8, 3, 5],
-        }),
-    ];
-};
-//-----наследование прототипа-----
-Student.prototype = Object.create(Human.prototype);
-Student.prototype.constructor = Student;
-
-Teacher.prototype = Object.create(Human.prototype); //создаёт новый объект с указанным прототипом и свойствами.
-Teacher.prototype.constructor = Teacher;
-
 Human.prototype = {
-    setFullName() {
-        return Object.assign(this.getFullName().split(" "));
+    setFullName(fullName) {
+        fullName = fullName.split(' ');
+        this.name = fullName[0];
+        this.surname = fullName[1];
     },
     getFullName() {
         return `${this.name} ${this.surname}`;
     },
-
 };
-//----------
+Student.prototype = Object.create(Human.prototype);
+Student.prototype.constructor = Student;
+Teacher.prototype = Object.create(Human.prototype);
+Teacher.prototype.constructor = Teacher;
+
 Teacher.prototype = Object.assign(Teacher.prototype, {
     getListOfNamesByAverageMark() {
-        return this.group.sort((a, b) => b.averageMark() - a.averageMark()).reduce(function (accum, cuurVal) {
-            accum += cuurVal.name + ' ';
-            return accum;
-        }, '').split(' ', this.group.length);
+        return this.group.sort((accum, cuurVal) => cuurVal.averageMark() - accum.averageMark()).map((cuurVal) => cuurVal.name);
     },
-
     getStudentByName(name) {
         return this.group.find((cuurVal) => cuurVal.name === name);
     },
@@ -84,46 +84,31 @@ Teacher.prototype = Object.assign(Teacher.prototype, {
 
 Student.prototype = Object.assign(Student.prototype, {
     averageMark() {
-        return this.marks.reduce((a, b) => a += b, 0) / this.marks.length;
+        return Math.round(this.marks.reduce((accum, cuurVal) => accum += cuurVal, 0) / this.marks.length);
     },
     minMark() {
-        return this.marks.sort((accum, cuurVal) => accum < cuurVal ? accum : cuurVal);
+        return this.marks.sort((accum, cuurVal) => cuurVal - accum)[marks.length - 1];
     },
     maxMark() {
-        return this.marks.sort((accum, cuurVal) => accum > cuurVal ? accum : cuurVal);
+        return this.marks.sort((accum, cuurVal) => accum - cuurVal)[marks.length - 1];
     },
     getFullName() {
-        return Human.prototype.getFullName().call(this) + ' - student';
+        return Human.prototype.getFullName.call(this) + ' - student';
     }
-
 });
 
-//--------human-footer----
-let s = new Human({
+let student = new Student(student1, student2, student3, student4, student5);
+let teacher = new Teacher({
+    name: 'David',
+    surname: 'Ferdison',
+    age: 34,
+    group
+});
+let human = new Human({
     name: "Sasha",
     surname: "Golden",
     age: 25
 });
-
-//------teacher-footer----
-let teacher = new Teacher({
-    name: 'Diana',
-    surname: 'Fox',
-    age: 34,
-});
-//-----student-----
-let student = new Student({
-    name: 'Pasha',
-    surname: 'Stark',
-    age: 19,
-    marks: [10, 1, 9, 5, 6]
-});
-
-
-//_________CONSOLE___________
-console.log(teacher.getListOfNamesByAverageMark());
-console.log(teacher.group);
-console.log(student);
-console.log(s);
-console.log(s.getFullName());
-console.log(s.setFullName());
+//console.log(human);
+//console.log(teacher);
+//console.log(student);
