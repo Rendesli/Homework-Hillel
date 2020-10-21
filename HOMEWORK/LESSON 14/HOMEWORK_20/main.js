d83f0709b34e5b3659b68e3c80b75eec6475895d
Rendesli <razinax2003@gmail.com>
2020-10-21 15:32:41 +0300

    listCommit

--- HOMEWORK/LESSON 14/HOMEWORK_20/main.js
+++ HOMEWORK/LESSON 14/HOMEWORK_20/main.js
@@ -1,75 +1,91 @@
-
-
-
-
 let ul = document.querySelector('#ul');
+
 ul.addEventListener('mousedown', function (e) {
-    e.preventDefault();
+  e.preventDefault();
 })
 
 ul.addEventListener('click', function (e) {
-    console.log(e);
+  console.log(e);
 
-    if (e.target == this) {
-        return false;
-    }
-    if (!e.ctrlKey) {
-        clearSelected(Array.from(this.children).filter(i => i !== e.target));
-    } 
-    addSelected(e.target);
+  if (e.target == this) {
+    return false;
+  }
+  if (!e.ctrlKey) {
+    clearSelected(Array.from(this.children).filter(i => i !== e.target));
+  }
+  addSelected(e.target);
 });
 
 function clearSelected(elems) {
-    for (let elem of elems) {
-        console.log(elem.classList.remove('selected'));
-    }
+  for (let elem of elems) {
+    console.log(elem.classList.remove('selected'));
+  }
 }
 
 function addSelected(target) {
-    target.classList.toggle('selected');
+  target.classList.toggle('selected');
 }
 
 
 
-
-
-let createLi = document.createElement('li');
 let menu = document.querySelector('#menu');
- let arrayTask = ['Убрать дома', 'Почитать книгу', 'Послушать музыку', 'Поиграть с братом', 'Сходить на курсы'];
-let rand=arrayTask[Math.floor(arrayTask.length * Math.random())];
-// let modal = document.getElementById('openModal');
+let arrayTask = ['Убрать дома', 'Почитать книгу', 'Послушать музыку', 'Поиграть с братом', 'Сходить на курсы', 'Помыть посуду', 'Погулять', 'Поиграть в игру', 'Выучить 10 слов на английском'];
+let rand;
+
 class Menu {
-    constructor(elem) {
-      this._elem = elem;
+  constructor(elem) {
+    this._elem = elem;
     elem.onclick = this.onClick.bind(this); // (*)
-
-    }
-
-    addToTheBeginning() {
-      createLi.textContent=`${rand}`;
-    ul.prepend(createLi);
-   // modal.classList.add('open');
-    }
-
-    addToTheEnd() {
-        createLi.textContent=`${rand}`;
-        ul.append(createLi);
-    }
-
-    removeLi() {
-
-    }
-    sortLi() {
-        alert('ищу');
-      }
-
-    onClick(event) {
-      let action = event.target.dataset.action;
-      if (action) {
-        this[action]();
-      }
-    };
   }
 
+  addToTheBeginning() {
+    let createLi = document.createElement('li');
+    addLi(createLi);
+    ul.prepend(createLi);
+
+  }
+
+  addToTheEnd() {
+    let createLi = document.createElement('li');
+    addLi(createLi);
+    ul.append(createLi);
+  }
+
+  removeLi() {
+    let classSelected = document.querySelectorAll('.selected');
+    classSelected.forEach(element => {
+      element.remove();
+    });
+  }
+
+  sortLi() {
+    let classSelected = document.querySelectorAll('.selected');
+    let array = Array.from(classSelected);
+    array = array.sort((firstEl, secondEl) => firstEl.textContent > secondEl.textContent ? 1 : -1);
+    [].forEach.call(ul.children, (elem) => {
+      if (elem.classList.contains('selected')) {
+        let ulSort = document.querySelector('#ulSort');
+        let createLi = document.createElement('li');
+        createLi.textContent = array.shift().textContent;
+        ulSort.append(createLi);
+      }
+    });
+  }
+
+  onClick(event) {
+    let action = event.target.dataset.action;
+    if (action) {
+      this[action]();
+    }
+  };
+}
+
+function addLi(createLi) {
+  for (let i = 0; i < arrayTask.length; i++) {
+    rand = arrayTask[Math.floor(arrayTask.length * Math.random())];
+    createLi.textContent = rand;
+
+  }
+}
 console.log(new Menu(menu));
-  
+
